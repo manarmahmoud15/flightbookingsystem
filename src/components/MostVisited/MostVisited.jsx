b5d60b8da31f91ee168daa382311f7e5109ca237
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-05-01 09:47:35 +0300

    last

--- src/components/MostVisited/MostVisited.jsx
+++ src/components/MostVisited/MostVisited.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import data from "../../data.json";
 import FlightCard from "../FlightCard/FlightCard";
-
 export default function MostVisited() {
   // const mostVisitedCards = document.querySelectorAll('.mostVisitedCard');
   const mostVisitedFlights = data.mostVisitedFlights;
